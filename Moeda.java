349ba4cbea3cd410ce01d3a3adb52d04f409dfd6
EdsonSuraty <edinhosuraty@gmail.com>
2025-07-16 15:20:58 -0300

    Adiciona: Cria filtro para buscar a cotação da moeda específica

--- Moeda.java
+++ Moeda.java
@@ -1,4 +1,22 @@
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public record Moeda(String base_code, Map<String, Double> conversation_rates) {
+public record Moeda(String base_code, Map<String, Double> conversion_rates) {
+
+    public Map<String, Double> getTaxasFiltradas(List<String> codigosMoedasDeInteresse) {
+
+        Map<String, Double> taxasFiltradas = new HashMap<>();
+
+        for (String codigo : codigosMoedasDeInteresse) {
+            // Verifica se o mapa original (conversion_rates) contém aquela chave
+            if (this.conversion_rates.containsKey(codigo)) {
+                // Se contém, pega o valor (a taxa) e coloca no novo mapa
+                taxasFiltradas.put(codigo, this.conversion_rates.get(codigo));
+            }
+        }
+
+        return taxasFiltradas;
+    }
+
 };
